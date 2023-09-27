3a3ecffa45e147e0c532221b029008568634d2e6
Everson-s8 <eversonx100@gmail.com>
2023-09-27 12:45:09 -0300

    Update

--- src/Arvore.java
+++ src/Arvore.java
@@ -11,6 +11,7 @@ public class Arvore<T extends Comparable<T>> {
 
         if (root == null) {
             root = valor;
+            root.pai = null;
         } else {
             No<T> atual = root;
             while (true) {
@@ -19,6 +20,7 @@ public class Arvore<T extends Comparable<T>> {
                         atual = atual.esquerda;
                     } else {
                         atual.esquerda = valor; // setando o valor na esquerda do atual
+                        atual.esquerda.pai = atual; // dizendo que o pai do novo valor setado é o atual
                         break;
                     }
                 } else { // se for maior ou igual
@@ -26,6 +28,7 @@ public class Arvore<T extends Comparable<T>> {
                         atual = atual.direita;
                     } else {
                         atual.direita = valor; // setando o valor na direita do atual
+                        atual.direita.pai = atual; // dizendo que o pai do novo valor setado é o atual
                         break;
                     }
                 }
@@ -34,12 +37,12 @@ public class Arvore<T extends Comparable<T>> {
     }
 
 
-    public boolean buscarElemento(T valor){
+    public No<T> buscarElemento(T valor){
         No<T> atual = root;
 
         while (atual != null){
             if (atual.dado == valor){
-                return true;
+                return atual;
             }else {
                 if (valor.compareTo(atual.dado) == -1){
                     atual = atual.esquerda;
@@ -47,23 +50,69 @@ public class Arvore<T extends Comparable<T>> {
                     atual = atual.direita;
                 }
             }
-        } return false;
+        } return null;
 
     }
+    // Na remoção existe 3 casos
+    // 1ª Caso
+    // Se não tiver filhos, no caso se for remover uma folha
+    // Sendo assim só tem que descobrir se o elemento que deseja ser removido está a esquerda ou direita do seu pai
+    // Após descobrir só setar a posição como null , mas no caso se for uma raiz tem que fazer um ajuste já que o pai é null
 
+    // 2º Caso
+    // Se tem filho apenas a direita ou esquerda
+    // nesse caso usa-se uma auxiliar para salvar o filho do No atual
+    // depois disso comparamos se o filho vai ficar a esquerda ou direita do pai
+    // depois seta o filho do No atual como filho do pai do No atual, assim substitui o no atual pelo seu filho removendo da lista
+    // tendo que também fazer um ajuste se for raiz
+
+    // 3ª Caso
+    // Se tem dois filhos
+    //
 
     public void remover(T elemento) throws Exception{
-        if (buscarElemento(elemento) == false){
+        if (buscarElemento(elemento) == null){
             throw new Exception("Elemento não existe na arvore");
         }else {
+            No<T> atual = buscarElemento(elemento);
+            No<T> filhosubstituto = null;
+            No<T> paiatual = atual.pai;
+            if (atual.direita != null && atual.esquerda != null){  // se tem dois filhos
 
+            } else if (atual.direita != null) { // se tem apenas um filho a direita
+
+                filhosubstituto = atual.direita;        // estou salvando o filho do atual que se encontra a direita
+                if (filhosubstituto.dado.compareTo(paiatual.dado) == -1){ // agora estou comparando se o filho é menor que o pai
+                    paiatual.esquerda = filhosubstituto;                  // se for menor eu seto ele a esquerda
+                }else{
+                    paiatual.direita = filhosubstituto;                   // se for maior eu seto ele a direita
+                }
+            } else if (atual.esquerda != null) { // se tem apenas um filho a esquerda
+                filhosubstituto = atual.esquerda;                             // salvando o filho que se encontra a esquerda
+                if (filhosubstituto.dado.compareTo(paiatual.dado) == -1){     // mesmo processo que acontece se tiver um filho a direita
+                    paiatual.esquerda = filhosubstituto;
+                }else{
+                    paiatual.direita = filhosubstituto;
+                }
+
+            }else { // se n tem filhos
+                if (atual.dado.compareTo(atual.pai.dado) == -1){ // está comparando se o valor está a direita ou a esquerda do seu pai
+                    paiatual.esquerda = null; // se estiver a direita então o pai está setando como null a sua direita removendo o elemento
+                }else {
+                    paiatual.direita = null; // se estiver a esquerda então o pai está setando como null a sua direita removendo o elemento
+                }
+            }
         }
 
 
     }
 
-    public void Ordem(){
-
+    public void emOrdem(No<T> no){
+        if (no != null){
+            emOrdem(no.esquerda);
+            System.out.println(no.dado);
+            emOrdem(no.direita);
+        }
     }
 
     public void preOrdem(){
@@ -72,5 +121,29 @@ public class Arvore<T extends Comparable<T>> {
     public void posOrdem(){
 
     }
+
+//    public void emOrdem(Elemento<TIPO> atual){
+//        if (atual != null){
+//            emOrdem(atual.getEsquerda());
+//            System.out.println(atual.getValor());
+//            emOrdem(atual.getDireita());
+//        }
+//    }
+//
+//    public void preOrdem(Elemento<TIPO> atual){
+//        if (atual != null){
+//            System.out.println(atual.getValor());
+//            preOrdem(atual.getEsquerda());
+//            preOrdem(atual.getDireita());
+//        }
+//    }
+//
+//    public void posOrdem(Elemento<TIPO> atual){
+//        if (atual != null){
+//            posOrdem(atual.getEsquerda());
+//            posOrdem(atual.getDireita());
+//            System.out.println(atual.getValor());
+//        }
+//    }
 
 }
--- src/No.java
+++ src/No.java
@@ -3,9 +3,42 @@ public class No<T>{
     public No<T> direita;
     public T dado;
     public No<T> esquerda;
+    public No<T> pai;
 
     public No(T valor){
         dado = valor;
-        direita = esquerda = null;
+        pai = direita = esquerda = null;
+    }
+
+    public No<T> getDireita() {
+        return direita;
+    }
+
+    public void setDireita(No<T> direita) {
+        this.direita = direita;
+    }
+
+    public T getDado() {
+        return dado;
+    }
+
+    public void setDado(T dado) {
+        this.dado = dado;
+    }
+
+    public No<T> getEsquerda() {
+        return esquerda;
+    }
+
+    public void setEsquerda(No<T> esquerda) {
+        this.esquerda = esquerda;
+    }
+
+    public No<T> getPai() {
+        return pai;
+    }
+
+    public void setPai(No<T> pai) {
+        this.pai = pai;
     }
 }
